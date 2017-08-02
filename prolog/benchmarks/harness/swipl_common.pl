12d422077a47b1922512d9d12ecc56aa2c191d81
samer-- <samer.abdallah.00@gmail.com>
2017-08-02 20:31:58 +0100

    Unbind result on second run; add time limit

--- prolog/benchmarks/harness/swipl_common.pl
+++ prolog/benchmarks/harness/swipl_common.pl
@@ -2,14 +2,16 @@ report(Show, Name, Time, Result) :-
    format('~w: ~`.t time = ~w ms~60|', [Name, Time]),
    (Show=1 -> format(' -- ~w\n', [Result]); nl).
 
-run_case(call(Goal,Res), answer(Res), Time) :-
-   T1 is cputime, call(Goal), T2 is cputime, Time is round(1000*(T2 - T1)).
-
-run_case(verify(Goal,Check), correct(R), Time) :-
-   T1 is cputime, call(Goal), T2 is cputime, Time is round(1000*(T2 - T1)),
-   (call(Check) -> R = true; R=false).
-
+run_case(call(Goal,Res), answer(Res), Time)    :- time(Goal, Time).
+run_case(verify(Goal,Check), correct(R), Time) :- time(Goal, Time), (call(Check) -> R = true; R=false).
 run_case(count(Goal), solutions(N), Time) :-
    run_case(call(findall(t,Goal,R), R), answer(Solns), Time),
    length(Solns,N).
 
+time(Goal,Time) :-
+   T1 is cputime, call(Goal),
+   T2 is cputime, Time is round(1000*(T2 - T1)).
+
+run_case_limited(Case, Result, Time) :-
+   catch( call_with_time_limit(60, run_tabled(run_case(Case,Result,Time))),
+          time_limit_exceeded, (Time=inf, Result=timeout)).
