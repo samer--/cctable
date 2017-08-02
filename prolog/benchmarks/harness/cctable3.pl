56c03f1aa3158de96192df3ab31691ddce7e2a76
samer-- <samer.abdallah.00@gmail.com>
2017-08-02 15:56:53 +0100

    Add cctable3 harness and more results

---
+++ prolog/benchmarks/harness/cctable3.pl
@@ -0,0 +1,11 @@
+:- use_module('../../cctable3').
+:- use_module('../../ccmacros').
+:- consult(swipl_common).
+:- current_prolog_flag(argv, [File|_]),
+   load_files(File,[]).
+
+main(Show) :-
+   case(Name, Case),
+   run_tabled(run_case(Case,Result,Time1)), garbage_collect,
+   run_tabled(run_case(Case,Result,Time2)),
+   report(Show, Name, [Time1,Time2], Result).
