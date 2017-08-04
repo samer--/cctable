2961213e9721e92ec81e9ca4e295ed4155f9ec12
samer-- <samer.abdallah.00@gmail.com>
2017-08-04 21:39:12 +0100

    Fix up first trie based version

--- prolog/cctable5.pl
+++ prolog/cctable5.pl
@@ -13,7 +13,6 @@
 :- use_module(library(delimcc), [p_reset/3, p_shift/2]).
 :- use_module(library(rbutils)).
 :- use_module(library(lambdaki)).
-:- use_module(library(trie)).
 
 
 %% cctabled(+Head:callable) is det.
@@ -29,9 +28,10 @@ cctabled(Head) :- p_shift(tab, Head).
 :- meta_predicate run_tabled(0).
 run_tabled(Goal) :-
    trie_new(Trie),
-   nb_setval(tabling_trie, Trie),
    term_variables(Goal, Ans),
-   run_tab(Goal, Ans).
+   setup_call_cleanup(nb_setval(tabling_trie, Trie),
+                      run_tab(Goal, Ans),
+                      nb_delete(tabling_trie)).
 
 head_to_variant_class(Head, VC) :-
    copy_term_nat(Head, VC),
@@ -50,20 +50,25 @@ cont_tab(susp(Head, Cont), Ans) :-
    -> trie_update(Trie, VC, tab(Solns,[K|Ks])), % !! potentially expensive copy here
       trie_gen(Solns, Y, _), run_tab(Cont, Ans) % should we copy Y or stick to grounds?
    ;  trie_new(Solns),
-      trie_insert(Trie, VC, tab(Solns,[]), TableHandle),
-      run_tab(producer(TableHandle, Solns, \Y^Head, K, Ans), Ans)
+      trie_insert(Trie, VC, tab(Solns,[])),
+      run_tab(producer(Trie, VC, Solns, \Y^Head, K, Ans), Ans)
    ).
 
 new_producer(new_producer, tab(Solns,[])) :- rb_empty(Solns).
 
-producer(Handle, Solns, Generate, KP, Ans) :-
+producer(Trie, VC, Solns, Generate, KP, Ans) :-
    call(Generate, Y),
    trie_insert(Solns, Y, t),
-   trie_term(Handle, tab(_,Ks)),
+   trie_lookup(Trie, VC, tab(_,Ks)), % nb, value is copied on lookup
    member(k(Y,Ans,Cont),[KP|Ks]), call(Cont).
 
 new_soln(Y, Ks, tab(Ys1,Ks), tab(Ys2,Ks)) :- rb_add(Y,t,Ys1,Ys2).
 
-get_tables(_).
-% get_tables(Tables) :- nb_dump(Raw), rb_map(Raw, sanitise, Tables).
-% sanitise(tab(S,_), SL) :- rb_keys(S,SL).
+get_tables(TablesTree) :-
+   nb_getval(tabling_trie,Trie),
+   findall(VC-SL, trie_variant_class_solutions(Trie, VC, SL), Tables),
+   list_to_rbtree(Tables, TablesTree).
+
+trie_variant_class_solutions(Trie, VC, Solns) :-
+   trie_gen(Trie, VC, tab(SolnsTrie, _)),
+   findall(S, trie_gen(SolnsTrie,S,_), Solns).
