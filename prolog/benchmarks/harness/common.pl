d1a6b2dd4a7e2426c3efa38e2fdc631d0c6eb1ec
samer-- <samer.abdallah.00@gmail.com>
2017-08-11 14:57:01 +0100

    Add common.pl to factor out benchmark results output

---
+++ prolog/benchmarks/harness/common.pl
@@ -0,0 +1,9 @@
+report(0, _Imp, Name, Time, _Result) :- format('~w: ~`.t time = ~w ms~60|\n', [Name, Time]).
+report(1, _Imp, Name, Time, Result) :- format('~w: ~`.t time = ~w ms~60| -- ~w\n', [Name, Time, Result]).
+report(2, Imp, Name, Times, _Result) :- 
+   last_time(Times,T),
+   format('run(~q, ~q, ~q).\n', [Imp, Name, T]).
+
+
+last_time(T,T) :- number(T), !.
+last_time(Ts,T) :- append(_,[T],Ts).
