8dce09be9818714628170cea3165a0ede68a01cb
samer-- <samer.abdallah.00@gmail.com>
2017-08-11 15:05:29 +0100

    Rename results predicate

--- prolog/benchmarks/harness/common.pl
+++ prolog/benchmarks/harness/common.pl
@@ -2,7 +2,7 @@ report(0, _Imp, Name, Time, _Result) :- format('~w: ~`.t time = ~w ms~60|\n', [N
 report(1, _Imp, Name, Time, Result) :- format('~w: ~`.t time = ~w ms~60| -- ~w\n', [Name, Time, Result]).
 report(2, Imp, Name, Times, _Result) :- 
    last_time(Times,T),
-   format('run(~q, ~q, ~q).\n', [Imp, Name, T]).
+   format('imp_case_time(~q, ~q, ~q).\n', [Imp, Name, T]).
 
 
 last_time(T,T) :- number(T), !.
