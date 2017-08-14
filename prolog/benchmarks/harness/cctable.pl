bb0e10750bd4a9bc3f2a969630c6c1d4f65e9e78
samer-- <samer.abdallah.00@gmail.com>
2017-08-14 17:39:07 +0100

    Increase timeout

--- prolog/benchmarks/harness/cctable.pl
+++ prolog/benchmarks/harness/cctable.pl
@@ -7,7 +7,7 @@
 
 main(Show) :-
    case(Name, Case),
-   run_case_limited(90, Case, _, Time1), !, garbage_collect,
-   run_case_limited(240, Case, Result, Time2),
+   run_case_limited(60, Case, _, Time1), !, garbage_collect,
+   run_case_limited(360, Case, Result, Time2),
    current_prolog_flag(argv, [Imp,_]),
    report(Show, Imp, Name, [Time1,Time2], Result).
